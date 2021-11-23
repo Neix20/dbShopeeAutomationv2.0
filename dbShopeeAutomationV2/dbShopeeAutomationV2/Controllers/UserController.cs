22c95cf5776b89a173ab8ee8d0e5fc276ca1582a
Neix20 <txen2000@gmail.com>
2021-11-23 23:44:56 +0800

    Completed User and User Role

---
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using dbShopeeAutomationV2.Models;
+using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbShopeeAutomationV2.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        dbShopeeAutomationV2Entities db = new dbShopeeAutomationV2Entities();
+
+        [ValidateInput(false)]
+        public ActionResult UserGridViewPartial()
+        {
+            var model = db.TShopeeUsers;
+            return PartialView("_UserGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult UserGridViewPartialAddNew(TShopeeUser item)
+        {
+            var model = db.TShopeeUsers;
+            return PartialView("_UserGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult UserGridViewPartialUpdate(TShopeeUser item)
+        {
+            var model = db.TShopeeUsers;
+            return PartialView("_UserGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult UserGridViewPartialDelete(int user_id)
+        {
+            var model = db.TShopeeUsers;
+            return PartialView("_UserGridViewPartial", model.ToList());
+        }
+    }
+}
---
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/UserRoleController.cs
@@ -0,0 +1,49 @@
+﻿using dbShopeeAutomationV2.Models;
+using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbShopeeAutomationV2.Controllers
+{
+    public class UserRoleController : Controller
+    {
+        // GET: UserRole
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        dbShopeeAutomationV2Entities db = new dbShopeeAutomationV2Entities();
+
+        [ValidateInput(false)]
+        public ActionResult UserRoleGridViewPartial()
+        {
+            var model = db.TShopeeUserRoles;
+            return PartialView("_UserRoleGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult UserRoleGridViewPartialAddNew(TShopeeUserRole item)
+        {
+            var model = db.TShopeeUserRoles;
+            return PartialView("_UserRoleGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult UserRoleGridViewPartialUpdate(TShopeeUserRole item)
+        {
+            var model = db.TShopeeUserRoles;
+            return PartialView("_UserRoleGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult UserRoleGridViewPartialDelete(int user_role_id)
+        {
+            var model = db.TShopeeUserRoles;
+            return PartialView("_UserRoleGridViewPartial", model.ToList());
+        }
+    }
+}

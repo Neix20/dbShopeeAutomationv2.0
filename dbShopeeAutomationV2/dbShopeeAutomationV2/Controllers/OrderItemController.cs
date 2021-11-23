ace5ac20e757887a970de755fa7e95bced5b6d52
Neix20 <txen2000@gmail.com>
2021-11-23 23:05:28 +0800

    Completed Order Item and Order Item Status

---
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/OrderItemController.cs
@@ -0,0 +1,49 @@
+﻿using dbShopeeAutomationV2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbShopeeAutomationV2.Controllers
+{
+    public class OrderItemController : Controller
+    {
+        // GET: OrderItem
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        dbShopeeAutomationV2Entities db = new dbShopeeAutomationV2Entities();
+
+        [ValidateInput(false)]
+        public ActionResult OrderItemGridViewPartial()
+        {
+            var model = db.TShopeeOrderItems;
+            return PartialView("~/Views/OrderItem/_OrderItemGridViewPartial.cshtml", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult OrderItemGridViewPartialAddNew(TShopeeOrderItem item)
+        {
+            var model = db.TShopeeOrderItems;
+            return PartialView("~/Views/OrderItem/_OrderItemGridViewPartial.cshtml", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult OrderItemGridViewPartialUpdate(TShopeeOrderItem item)
+        {
+            var model = db.TShopeeOrderItems;
+            return PartialView("~/Views/OrderItem/_OrderItemGridViewPartial.cshtml", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult OrderItemGridViewPartialDelete(int order_item_id)
+        {
+            var model = db.TShopeeOrderItems;
+            return PartialView("~/Views/OrderItem/_OrderItemGridViewPartial.cshtml", model.ToList());
+        }
+
+    }
+}
---
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/OrderItemStatusController.cs
@@ -0,0 +1,49 @@
+﻿using dbShopeeAutomationV2.Models;
+using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbShopeeAutomationV2.Controllers
+{
+    public class OrderItemStatusController : Controller
+    {
+        // GET: OrderItemStatus
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        dbShopeeAutomationV2Entities db = new dbShopeeAutomationV2Entities();
+
+        [ValidateInput(false)]
+        public ActionResult OrderItemStatusGridViewPartial()
+        {
+            var model = db.TShopeeOrderItemStatus;
+            return PartialView("_OrderItemStatusGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult OrderItemStatusGridViewPartialAddNew(TShopeeOrderItemStatu item)
+        {
+            var model = db.TShopeeOrderItemStatus;
+            return PartialView("_OrderItemStatusGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult OrderItemStatusGridViewPartialUpdate(TShopeeOrderItemStatu item)
+        {
+            var model = db.TShopeeOrderItemStatus;
+            return PartialView("_OrderItemStatusGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult OrderItemStatusGridViewPartialDelete(int order_item_status_id)
+        {
+            var model = db.TShopeeOrderItemStatus;
+            return PartialView("_OrderItemStatusGridViewPartial", model.ToList());
+        }
+    }
+}

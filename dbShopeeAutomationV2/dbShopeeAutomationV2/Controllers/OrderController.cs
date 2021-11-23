67d7e313c63d336a2711500046b8013714c44ece
Neix20 <txen2000@gmail.com>
2021-11-23 22:35:57 +0800

    Completed Order and Order Status

---
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/OrderController.cs
@@ -0,0 +1,49 @@
+﻿using dbShopeeAutomationV2.Models;
+using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbShopeeAutomationV2.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        dbShopeeAutomationV2Entities db = new dbShopeeAutomationV2Entities();
+
+        [ValidateInput(false)]
+        public ActionResult OrderGridViewPartial()
+        {
+            var model = db.TShopeeOrders;
+            return PartialView("_OrderGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult OrderGridViewPartialAddNew(TShopeeOrder item)
+        {
+            var model = db.TShopeeOrders;
+            return PartialView("_OrderGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult OrderGridViewPartialUpdate(TShopeeOrder item)
+        {
+            var model = db.TShopeeOrders;
+            return PartialView("_OrderGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult OrderGridViewPartialDelete(int order_id)
+        {
+            var model = db.TShopeeOrders;
+            return PartialView("_OrderGridViewPartial", model.ToList());
+        }
+    }
+}
---
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/OrderStatusController.cs
@@ -0,0 +1,49 @@
+﻿using dbShopeeAutomationV2.Models;
+using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbShopeeAutomationV2.Controllers
+{
+    public class OrderStatusController : Controller
+    {
+        // GET: OrderStatus
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        dbShopeeAutomationV2Entities db = new dbShopeeAutomationV2Entities();
+
+        [ValidateInput(false)]
+        public ActionResult OrderStatusGridViewPartial()
+        {
+            var model = db.TShopeeOrderStatus;
+            return PartialView("_OrderStatusGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult OrderStatusGridViewPartialAddNew(TShopeeOrderStatu item)
+        {
+            var model = db.TShopeeOrderStatus;
+            return PartialView("_OrderStatusGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult OrderStatusGridViewPartialUpdate(TShopeeOrderStatu item)
+        {
+            var model = db.TShopeeOrderStatus;
+            return PartialView("_OrderStatusGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult OrderStatusGridViewPartialDelete(int order_status_id)
+        {
+            var model = db.TShopeeOrderStatus;
+            return PartialView("_OrderStatusGridViewPartial", model.ToList());
+        }
+    }
+}

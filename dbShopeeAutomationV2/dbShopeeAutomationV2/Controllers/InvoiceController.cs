572b7003cf5ea61e2dd1a66a9b50e7b9f5206bb1
Neix20 <txen2000@gmail.com>
2021-11-23 23:26:19 +0800

    Completed Invoice and Invoice Status

---
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/InvoiceController.cs
@@ -0,0 +1,49 @@
+﻿using dbShopeeAutomationV2.Models;
+using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbShopeeAutomationV2.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        // GET: Invoice
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        dbShopeeAutomationV2Entities db = new dbShopeeAutomationV2Entities();
+
+        [ValidateInput(false)]
+        public ActionResult InvoiceGridViewPartial()
+        {
+            var model = db.TShopeeInvoices;
+            return PartialView("_InvoiceGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult InvoiceGridViewPartialAddNew(TShopeeInvoice item)
+        {
+            var model = db.TShopeeInvoices;
+            return PartialView("_InvoiceGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult InvoiceGridViewPartialUpdate(TShopeeInvoice item)
+        {
+            var model = db.TShopeeInvoices;
+            return PartialView("_InvoiceGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult InvoiceGridViewPartialDelete(int invoice_id)
+        {
+            var model = db.TShopeeInvoices;
+            return PartialView("_InvoiceGridViewPartial", model.ToList());
+        }
+    }
+}
---
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/InvoiceStatusController.cs
@@ -0,0 +1,49 @@
+﻿using dbShopeeAutomationV2.Models;
+using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbShopeeAutomationV2.Controllers
+{
+    public class InvoiceStatusController : Controller
+    {
+        // GET: InvoiceStatus
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        dbShopeeAutomationV2Entities db = new dbShopeeAutomationV2Entities();
+
+        [ValidateInput(false)]
+        public ActionResult InvoiceStatusGridViewPartial()
+        {
+            var model = db.TShopeeInvoiceStatus;
+            return PartialView("_InvoiceStatusGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult InvoiceStatusGridViewPartialAddNew(TShopeeInvoiceStatu item)
+        {
+            var model = db.TShopeeInvoiceStatus;
+            return PartialView("_InvoiceStatusGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult InvoiceStatusGridViewPartialUpdate(TShopeeInvoiceStatu item)
+        {
+            var model = db.TShopeeInvoiceStatus;
+            return PartialView("_InvoiceStatusGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult InvoiceStatusGridViewPartialDelete(int invoice_status_id)
+        {
+            var model = db.TShopeeInvoiceStatus;
+            return PartialView("_InvoiceStatusGridViewPartial", model.ToList());
+        }
+    }
+}

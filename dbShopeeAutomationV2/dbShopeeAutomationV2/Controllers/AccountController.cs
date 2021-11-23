37cc5642b2150b38d40fc5dad8abf1d6ef0c0529
Neix20 <txen2000@gmail.com>
2021-11-23 10:56:56 +0800

    Completed ORM using Entity Framework
    Populated dbShopeeAutomationModels

---
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbShopeeAutomationV2.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbShopeeAutomationV2.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public abstract class AdminController : Controller
+    {
+    }
+}

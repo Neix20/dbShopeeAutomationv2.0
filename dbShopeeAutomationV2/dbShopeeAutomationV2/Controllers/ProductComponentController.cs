ffd6d286a556b1cd53a9334a614fc0506f9dfa6e
Neix20 <txen2000@gmail.com>
2021-12-17 16:29:05 +0800

    Completed Product Component

--- dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/ProductComponentController.cs
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/ProductComponentController.cs
@@ -1,4 +1,5 @@
-﻿using DevExpress.Web.Mvc;
+﻿using dbShopeeAutomationV2.Models;
+using DevExpress.Web.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,53 @@ namespace dbShopeeAutomationV2.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        dbShopeeAutomationV2Entities db = new dbShopeeAutomationV2Entities();
+
+        [ValidateInput(false)]
+        public ActionResult ProductComponentGridViewPartial()
+        {
+            var model = db.TShopeeProductComponents;
+            return PartialView("_ProductComponentGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult ProductComponentGridViewPartialAddNew(TShopeeProductComponent item)
+        {
+            string username = User.Identity.Name;
+
+            item.quantity = (item.quantity == null) ? 0 : item.quantity;
+
+            dbStoredProcedure.productComponentInsert(item.master_product_id, item.sub_product_id, item.quantity, username);
+            db.SaveChanges();
+
+            var model = db.TShopeeProductComponents;
+            return PartialView("_ProductComponentGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult ProductComponentGridViewPartialUpdate(TShopeeProductComponent item)
+        {
+            string username = User.Identity.Name;
+
+            item.quantity = (item.quantity == null) ? 0 : item.quantity;
+
+            dbStoredProcedure.productComponentUpdate(item.product_component_id, item.master_product_id, item.sub_product_id, item.quantity, username);
+            db.SaveChanges();
+
+            var model = db.TShopeeProductComponents;
+            return PartialView("_ProductComponentGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult ProductComponentGridViewPartialDelete(int product_component_id)
+        {
+            dbStoredProcedure.productComponentDelete(product_component_id);
+            db.SaveChanges();
+
+            var model = db.TShopeeProductComponents;
+            return PartialView("_ProductComponentGridViewPartial", model.ToList());
         }
     }
 }

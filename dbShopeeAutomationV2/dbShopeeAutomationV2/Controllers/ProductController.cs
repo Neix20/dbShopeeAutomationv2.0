ef66765e36579df91479ae3bb6200aaad49e5b93
Neix20 <txen2000@gmail.com>
2021-12-20 10:51:50 +0800

    Changed Name of Controller
    Change Controller from ProductionV2 to Production
    Change Controller from ProductV2 to Product

---
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/ProductController.cs
@@ -0,0 +1,98 @@
+﻿using dbShopeeAutomationV2.Models;
+using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbShopeeAutomationV2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        dbShopeeAutomationV2Entities db = new dbShopeeAutomationV2Entities();
+
+        [ValidateInput(false)]
+        public ActionResult ProductGridViewPartial()
+        {
+            var model = db.TShopeeProducts;
+            return PartialView("_ProductGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult ProductGridViewPartialAddNew(TShopeeProduct item)
+        {
+            string username = User.Identity.Name;
+
+            item.product_code = (item.product_code == null || item.product_code.Equals("")) ? generalFunc.Random10DigitCode() : item.product_code;
+            item.name = (item.name == null) ? "product_name" : item.name;
+            item.description = (item.description == null) ? "product_description" : item.description;
+
+            string product_brand = db.TShopeeProductBrands.FirstOrDefault(it => it.product_brand_id == item.product_brand_id).name;
+            string product_type = db.TShopeeProductTypes.FirstOrDefault(it => it.product_type_id == item.product_type_id).name;
+            string product_variety = db.TShopeeProductVarieties.FirstOrDefault(it => it.product_variety_id == item.product_variety_id).name;
+            item.SKU = (item.SKU == null) ? generalFunc.GenSKU(product_brand, product_type, product_variety) : item.SKU;
+            item.SKU2 = (item.SKU2 == null) ? $"{item.SKU}2" : item.SKU2;
+
+            item.buy_price = (item.buy_price == null) ? 0 : item.buy_price;
+            item.sell_price = (item.sell_price == null) ? 0 : item.sell_price;
+
+            dbStoredProcedure.productInsert(
+                item.product_code, item.name,
+                item.description, item.SKU, item.SKU2,
+                item.buy_price, item.sell_price,
+                item.product_brand_id, item.product_model_id, item.product_category_id,
+                item.product_type_id, item.product_variety_id, item.product_status_id, username);
+            db.SaveChanges();
+
+            var model = db.TShopeeProducts;
+            return PartialView("_ProductGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult ProductGridViewPartialUpdate(TShopeeProduct item)
+        {
+            string username = User.Identity.Name;
+
+            item.product_code = (item.product_code == null || item.product_code.Equals("")) ? generalFunc.Random10DigitCode() : item.product_code;
+            item.name = (item.name == null) ? "product_name" : item.name;
+            item.description = (item.description == null) ? "product_description" : item.description;
+
+            string product_brand = db.TShopeeProductBrands.FirstOrDefault(it => it.product_brand_id == item.product_brand_id).name;
+            string product_type = db.TShopeeProductTypes.FirstOrDefault(it => it.product_type_id == item.product_type_id).name;
+            string product_variety = db.TShopeeProductVarieties.FirstOrDefault(it => it.product_variety_id == item.product_variety_id).name;
+            item.SKU = (item.SKU == null) ? generalFunc.GenSKU(product_brand, product_type, product_variety) : item.SKU;
+            item.SKU2 = (item.SKU2 == null) ? $"{item.SKU}2" : item.SKU2;
+
+            item.buy_price = (item.buy_price == null) ? 0 : item.buy_price;
+            item.sell_price = (item.sell_price == null) ? 0 : item.sell_price;
+
+            dbStoredProcedure.productUpdate(
+                item.product_id, item.product_code, item.name,
+                item.description, item.SKU, item.SKU2,
+                item.buy_price, item.sell_price,
+                item.product_brand_id, item.product_model_id, item.product_category_id,
+                item.product_type_id, item.product_variety_id, item.product_status_id, username);
+            db.SaveChanges();
+
+            var model = db.TShopeeProducts;
+            return PartialView("_ProductGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult ProductGridViewPartialDelete(int product_id)
+        {
+            dbStoredProcedure.productDelete(product_id);
+            db.SaveChanges();
+
+            var model = db.TShopeeProducts;
+            return PartialView("_ProductGridViewPartial", model.ToList());
+        }
+    }
+}
--- dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/ProductExcelController.cs
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/ProductExcelController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using IronXL;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using dbShopeeAutomationV2.Models;
 
 namespace dbShopeeAutomationV2.Controllers
 {
@@ -15,9 +17,77 @@ namespace dbShopeeAutomationV2.Controllers
             return View();
         }
 
+        dbShopeeAutomationV2Entities db = new dbShopeeAutomationV2Entities();
+
+        public string readExcelFile(string fileName, string username)
+        {
+            string str = "";
+            // Read 'Supplier Info' Worksheet
+            str += getSupplierInfo(fileName, username);
+            //str += getRawMaterialTrackingInfo(fileName, username);
+
+
+            return $"<table>{str}</table>";
+        }
+
+        // 1.
+        public string getSupplierInfo(string fileName, string username)
+        {
+            string str = "";
+
+            WorkBook wb = WorkBook.Load(fileName);
+            WorkSheet ws = wb.GetWorkSheet("Supplier Info");
+
+            // Get Only Rows without headers
+            foreach(var row in ws.Rows.ToList().GetRange(1, ws.RowCount - 1))
+            {
+                var tmp_arr = row.ToArray();
+
+                string supplier_name = (string)tmp_arr[1].Value;
+                string supplier_nation = (string)tmp_arr[2].Value;
+                string supplier_code = (string)tmp_arr[3].Value;
+
+                if (supplier_name == "" || supplier_name.Equals("Supplier")) continue;
+
+                dbStoredProcedure.supplierInsert(supplier_name, supplier_code, supplier_nation, username);
+            }
+            db.SaveChanges();
+
+            return str;
+        }
+
+        // 2.
+        //public string getRawMaterialTrackingInfo(string fileName, string username)
+        //{
+        //    string str = "";
+
+        //    WorkBook wb = WorkBook.Load(fileName);
+        //    WorkSheet ws = wb.GetWorkSheet("Raw Material Tracking Info");
+
+        //    //// Get Only Rows without headers
+        //    //foreach (var row in ws.Rows.ToList().GetRange(5, ws.RowCount - 1))
+        //    //{
+        //    //    var tmp_arr = row.ToArray();
+
+        //    //    string supplier_name = (string)tmp_arr[1].Value;
+        //    //    string supplier_nation = (string)tmp_arr[2].Value;
+        //    //    string supplier_code = (string)tmp_arr[3].Value;
+
+        //    //    if (supplier_name == "" || supplier_name.Equals("Supplier")) continue;
+
+        //    //    dbStoredProcedure.supplierInsert(supplier_name, supplier_code, supplier_nation, username);
+        //    //}
+        //    //db.SaveChanges();
+
+        //    str += String.Join("", ws.Rows.ToList().Select(x => $"<tr>{String.Join("", x.ToList().Select(y => $"<td>{y}</td>").ToArray())}<tr>"));
+        //    return str;
+        //}
+
         [HttpPost]
         public ActionResult uploadFile()
         {
+            string username = User.Identity.Name;
+
             var file = Request.Files["fileUpload"];
 
             if (file != null && file.ContentLength > 0)
@@ -30,7 +100,7 @@ namespace dbShopeeAutomationV2.Controllers
                 if (System.IO.File.Exists(file_path)) System.IO.File.Delete(file_path);
                 file.SaveAs(file_path);
 
-                //readExcelFile(file_path, platform_id, username);
+                readExcelFile(file_path, username);
 
                 return Content($"File {file.FileName} Uploaded Successfully!");
             }

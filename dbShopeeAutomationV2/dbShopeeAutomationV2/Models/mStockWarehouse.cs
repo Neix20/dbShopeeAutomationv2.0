02495cefdd133474ff5d771bd02bcc42c7b0baa0
Neix20 <txen2000@gmail.com>
2021-12-23 17:30:07 +0800

    Made Minor Changes

---
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Models/mStockWarehouse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dbShopeeAutomationV2.Models
+{
+    public class mStockWarehouse
+    {
+        public mStockWarehouse(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+
+        public mStockWarehouse()
+        {
+
+        }
+
+        public string name { get; set; }
+        public string address { get; set; }
+    }
+}

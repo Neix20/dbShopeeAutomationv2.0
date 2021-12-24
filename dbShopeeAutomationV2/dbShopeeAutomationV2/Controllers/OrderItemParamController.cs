11db985fd3289fbabd2eaf0727cba84230445ade
Neix20 <txen2000@gmail.com>
2021-12-24 10:15:21 +0800

    Completed Order Form

---
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Controllers/OrderItemParamController.cs
@@ -0,0 +1,49 @@
+﻿using dbShopeeAutomationV2.Models;
+using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dbShopeeAutomationV2.Controllers
+{
+    public class OrderItemParamController : Controller
+    {
+        // GET: OrderItemParam
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        dbShopeeAutomationV2Entities db = new dbShopeeAutomationV2Entities();
+
+        [ValidateInput(false)]
+        public ActionResult OrderItemParamGridViewPartial()
+        {
+            var model = db.TShopeeOrderItems;
+            return PartialView("_OrderItemParamGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult OrderItemParamGridViewPartialAddNew(TShopeeOrderItem item)
+        {
+            var model = db.TShopeeOrderItems;
+            return PartialView("_OrderItemParamGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult OrderItemParamGridViewPartialUpdate(TShopeeOrderItem item)
+        {
+            var model = db.TShopeeOrderItems;
+            return PartialView("_OrderItemParamGridViewPartial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult OrderItemParamGridViewPartialDelete(int order_item_id)
+        {
+            var model = db.TShopeeOrderItems;
+            return PartialView("_OrderItemParamGridViewPartial", model.ToList());
+        }
+    }
+}

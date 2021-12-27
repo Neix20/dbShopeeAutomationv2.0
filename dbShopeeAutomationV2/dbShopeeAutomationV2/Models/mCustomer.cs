e063bafab3f2330568c2470567dcfa7180678b0e
Neix20 <txen2000@gmail.com>
2021-12-27 13:52:23 +0800

    Completed Invoice

---
+++ dbShopeeAutomationV2/dbShopeeAutomationV2/Models/mCustomer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dbShopeeAutomationV2.Models
+{
+    public class mCustomer
+    {
+        public mCustomer(int customer_id, string name)
+        {
+            this.customer_id = customer_id;
+            this.name = name;
+        }
+
+        public mCustomer()
+        {
+
+        }
+
+        public int customer_id { get; set; }
+        public string name { get; set; }
+    }
+}
